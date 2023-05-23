2460f0ae1ab6262bc747c7cdab7d6f5c6849b3d3
tetyaZyna <sv2408sv@gmail.com>
2023-05-23 16:15:59 +0200

    Initial commit

---
+++ src/main/java/com/project/japaneseBot/JapaneseBotApplication.java
@@ -0,0 +1,13 @@
+package com.project.japaneseBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapaneseBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JapaneseBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/japaneseBot/JapaneseBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.japaneseBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JapaneseBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
