0b65db41ef23e5038db2d7bcd21f190bcac0bfa7
tetyaZyna <sv2408sv@gmail.com>
2023-07-27 14:14:01 +0200

    Delete methods which use KatakanaRepository and HiraganaRepository

--- src/main/java/com/project/japaneseBot/bot/buttons/Keyboards.java
+++ src/main/java/com/project/japaneseBot/bot/buttons/Keyboards.java
@@ -3,8 +3,6 @@ package com.project.japaneseBot.bot.buttons;
 import com.project.japaneseBot.alphabet.model.entity.AlphabetsEntity;
 import com.project.japaneseBot.alphabet.model.enums.AlphabetsTypes;
 import com.project.japaneseBot.alphabet.repository.AlphabetsRepository;
-import com.project.japaneseBot.alphabet.repository.ReadOnlyHiraganaRepository;
-import com.project.japaneseBot.alphabet.repository.ReadOnlyKatakanaRepository;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
@@ -15,56 +13,22 @@ import java.util.List;
 
 @Component
 public class Keyboards {
-    @Deprecated
-    private final ReadOnlyKatakanaRepository katakanaRepository;
-    @Deprecated
-    private final ReadOnlyHiraganaRepository hiraganaRepository;
     private final AlphabetsRepository alphabetsRepository;
     private ReplyKeyboardMarkup katakanaReplyKeyboardMarkup;
     private ReplyKeyboardMarkup hiraganaReplyKeyboardMarkup;
 
     @Deprecated
-    public Keyboards (ReadOnlyKatakanaRepository katakanaRepository, ReadOnlyHiraganaRepository hiraganaRepository, AlphabetsRepository alphabetsRepository) {
-        this.katakanaRepository = katakanaRepository;
-        this.hiraganaRepository = hiraganaRepository;
+    public Keyboards(AlphabetsRepository alphabetsRepository) {
         this.alphabetsRepository = alphabetsRepository;
         initializeAlphabetsReplyKeyboards();
     }
 
-    public ReplyKeyboardMarkup getKatakanaReplyKeyboard () {
+    public ReplyKeyboardMarkup getKatakanaReplyKeyboard() {
         return katakanaReplyKeyboardMarkup;
     }
 
-    public ReplyKeyboardMarkup getHiraganaReplyKeyboard () {
+    public ReplyKeyboardMarkup getHiraganaReplyKeyboard() {
         return hiraganaReplyKeyboardMarkup;
-    }
-
-    @Deprecated
-    private void initializeKatakanaReplyKeyboard() {
-        katakanaReplyKeyboardMarkup = new ReplyKeyboardMarkup();
-        katakanaReplyKeyboardMarkup.setResizeKeyboard(true);
-        katakanaReplyKeyboardMarkup.setSelective(true);
-        List<KeyboardRow> keyboardRowList = new ArrayList<>();
-        KeyboardRow keyboardRows = new KeyboardRow();
-        String katakana;
-        long repositoryLength = katakanaRepository.count();
-        for (long i = 1; i <= repositoryLength; i++) {
-            if (i % 10 == 1 && i > 1) {
-                keyboardRowList.add(keyboardRows);
-                keyboardRows = new KeyboardRow();
-            }
-            if (katakanaRepository.existsById(i)) {
-                katakana = katakanaRepository.findByHieroglyphId(i)
-                        .orElseThrow(RuntimeException::new)
-                        .getHieroglyph();
-                KeyboardButton button = new KeyboardButton(katakana);
-                keyboardRows.add(button);
-            }
-        }
-        KeyboardButton switchButton = new KeyboardButton("/hiragana");
-        keyboardRows.add(switchButton);
-        keyboardRowList.add(keyboardRows);
-        katakanaReplyKeyboardMarkup.setKeyboard(keyboardRowList);
     }
 
     private void initializeAlphabetsReplyKeyboards() {
@@ -111,33 +75,5 @@ public class Keyboards {
         KeyboardButton navigationButton = new KeyboardButton(buttonText);
         navigationRow.add(navigationButton);
         keyboardRowList.add(navigationRow);
-    }
-
-    @Deprecated
-    private void initializeHiraganaReplyKeyboard() {
-        hiraganaReplyKeyboardMarkup = new ReplyKeyboardMarkup();
-        hiraganaReplyKeyboardMarkup.setResizeKeyboard(true);
-        hiraganaReplyKeyboardMarkup.setSelective(true);
-        List<KeyboardRow> keyboardRowList = new ArrayList<>();
-        KeyboardRow keyboardRows = new KeyboardRow();
-        String hiragana;
-        long repositoryLength = hiraganaRepository.count();
-        for (long i = 1; i <= repositoryLength; i++) {
-            if (i % 10 == 1 && i > 1) {
-                keyboardRowList.add(keyboardRows);
-                keyboardRows = new KeyboardRow();
-            }
-            if (hiraganaRepository.existsById(i)) {
-                hiragana = hiraganaRepository.findByHieroglyphId(i)
-                        .orElseThrow(RuntimeException::new)
-                        .getHieroglyph();
-                KeyboardButton button = new KeyboardButton(hiragana);
-                keyboardRows.add(button);
-            }
-        }
-        KeyboardButton switchButton = new KeyboardButton("/katakana");
-        keyboardRows.add(switchButton);
-        keyboardRowList.add(keyboardRows);
-        hiraganaReplyKeyboardMarkup.setKeyboard(keyboardRowList);
     }
 }
