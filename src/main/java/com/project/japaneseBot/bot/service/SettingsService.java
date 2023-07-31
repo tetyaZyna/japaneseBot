8e197adaa15f53029e65cc2d59ec08deddf9e4d1
tetyaZyna <sv2408sv@gmail.com>
2023-07-31 16:03:49 +0200

    Add basic validation

--- src/main/java/com/project/japaneseBot/bot/service/SettingsService.java
+++ src/main/java/com/project/japaneseBot/bot/service/SettingsService.java
@@ -6,12 +6,14 @@ import com.project.japaneseBot.task.repository.TaskRepository;
 import com.project.japaneseBot.task.repository.TaskSettingsRepository;
 import com.project.japaneseBot.user.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class SettingsService {
     UserRepository userRepository;
     TaskRepository taskRepository;
@@ -25,13 +27,29 @@ public class SettingsService {
 
     public void createAndSaveSettings(String receivedMessage) {
         List<String> receivedSettings = List.of(receivedMessage.split(" "));
-        settingsRepository.save(TaskSettingsEntity.builder()
-                .settingsName(receivedSettings.get(1))
-                .questionCount(Integer.parseInt(receivedSettings.get(2)))
-                .alphabet(receivedSettings.get(3))
-                .letterGroup("ALL")
-                .usePronouns(Boolean.parseBoolean(receivedSettings.get(4)))
-                .useLetters(Boolean.parseBoolean(receivedSettings.get(5)))
-                .build());
+        String settingsName;
+        int questionCount;
+        String alphabet;
+        boolean usePronouns;
+        boolean useLetters;
+        try {
+            settingsName = receivedSettings.get(1);
+            questionCount = Integer.parseInt(receivedSettings.get(2));
+            alphabet = receivedSettings.get(3);
+            usePronouns = Boolean.parseBoolean(receivedSettings.get(4));
+            useLetters = Boolean.parseBoolean(receivedSettings.get(5));
+
+            settingsRepository.save(TaskSettingsEntity.builder()
+                    .settingsName(settingsName)
+                    .questionCount(questionCount)
+                    .alphabet(alphabet)
+                    .letterGroup("ALL")
+                    .usePronouns(usePronouns)
+                    .useLetters(useLetters)
+                    .build());
+        } catch (RuntimeException e) {
+            log.error(e.getMessage());
+        }
+
     }
 }
